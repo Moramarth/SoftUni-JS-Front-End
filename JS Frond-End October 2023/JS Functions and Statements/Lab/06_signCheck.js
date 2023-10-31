70abd07efdcad70aff0005e439be1356f29f6016
George Lazarov <122516587+Moramarth@users.noreply.github.com>
2023-10-31 18:26:32 +0200

    Task Sign Check refactor

--- JS Frond-End October 2023/JS Functions and Statements/Lab/06_signCheck.js
+++ JS Frond-End October 2023/JS Functions and Statements/Lab/06_signCheck.js
@@ -3,14 +3,10 @@ function signCheck(firstNumber, secondNumber, thirdNumber) {
     let numbers = [firstNumber, secondNumber, thirdNumber]
     let negativeCount = 0;
 
-    if (numbers.every(number => number < 0)) console.log("Negative");
-    else if (numbers.every(number => number > 0)) console.log("Positive");
-    else {
-        for (number of numbers) {
-            if (number < 0) negativeCount += 1;
-        }
-
-        if (negativeCount === 1) console.log("Negative");
-        else console.log("Positive");
+    for (number of numbers) {
+        if (number < 0) negativeCount += 1;
     }
+
+    if (negativeCount % 2 === 1) console.log("Negative");
+    else console.log("Positive");
 }
