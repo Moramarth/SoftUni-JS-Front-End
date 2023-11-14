3a53c37b29678066d90cb2395b0d845d3e4fd7de
George Lazarov <122516587+Moramarth@users.noreply.github.com>
2023-11-14 23:07:27 +0200

    JS DOM Events - Exercise
    Added solutions for first two tasks

--- JS Frond-End October 2023/JS DOM Events/Exercise/01. Sections/script.js
+++ JS Frond-End October 2023/JS DOM Events/Exercise/01. Sections/script.js
@@ -1,3 +1,21 @@
 function create(words) {
-   console.log('TODO:...');
+
+   let container = document.getElementById("content");
+
+   for (let word of words) {
+      let currentParagraph = document.createElement("p");
+      let currentDiv = document.createElement("div");
+
+      currentParagraph.textContent = word;
+      currentParagraph.style.display = "none";
+
+      currentDiv.appendChild(currentParagraph);
+      currentDiv.addEventListener("click", makeTextVisible);
+
+      container.appendChild(currentDiv);
+   };
+
+   function makeTextVisible(event) {
+      event.target.children[0].style.display = "block"
+   };
 }
--- JS Frond-End October 2023/JS DOM Events/Exercise/02. Time-Converter/solution.js
+++ JS Frond-End October 2023/JS DOM Events/Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,55 @@
 function attachEventsListeners() {
+    const converter = {
+        days: (value, type) => {
+            if (type === "days") return value;
+            if (type === "hours") return value / 24;
+            if (type === "minutes") return value / 1440;
+            if (type === "seconds") return value / 86400;
+        },
 
-    console.log('TODO:...');
+        hours: (value, type) => {
+            if (type === "days") return value * 24;
+            if (type === "hours") return value;
+            if (type === "minutes") return value / 60;
+            if (type === "seconds") return value / 3600;
+        },
+
+        minutes: (value, type) => {
+            if (type === "days") return value * 1440;
+            if (type === "hours") return value * 60;
+            if (type === "minutes") return value;
+            if (type === "seconds") return value / 60;
+        },
+
+        seconds: (value, type) => {
+            if (type === "days") return value * 86400;
+            if (type === "hours") return value * 3600;
+            if (type === "minutes") return value * 60;
+            if (type === "seconds") return value;
+        },
+    }
+    
+    let buttons = Array.from(document.querySelectorAll("input[type=button]"));
+
+    for (let button of buttons) {
+        button.addEventListener("click", handleButtonClick);
+    };
+
+
+    function handleButtonClick(event) {
+        let currentButton = event.target;
+        let dataField = currentButton.parentElement.children[1];
+
+        let inputValue = Number(dataField.value);
+        let timeUnit = dataField.id;
+
+        converTimeUnits(inputValue, timeUnit);
+    }
+
+    function converTimeUnits(value, type) {
+        document.getElementById("days").value = converter.days(value, type);
+        document.getElementById("hours").value = converter.hours(value, type);
+        document.getElementById("minutes").value = converter.minutes(value, type);
+        document.getElementById("seconds").value = converter.seconds(value, type);
+    }
 }
