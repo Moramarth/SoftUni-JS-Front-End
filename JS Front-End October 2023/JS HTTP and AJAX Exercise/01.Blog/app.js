3b23149afdad103be32355afa9b1124b58e5ae88
George Lazarov <122516587+Moramarth@users.noreply.github.com>
2023-11-19 09:59:44 +0200

    JS HTTP and AJAX - Exercise

--- JS Front-End October 2023/JS HTTP and AJAX Exercise/01.Blog/app.js
+++ JS Front-End October 2023/JS HTTP and AJAX Exercise/01.Blog/app.js
@@ -1,5 +1,54 @@
+const apiURL = "http://localhost:3030/jsonstore/blog/";
+const selectMenu = document.querySelector("#posts");
+const postInfoOutput = document.querySelector("#post-body");
+const commentsList = document.querySelector("#post-comments");
+const postTitleHeading = document.querySelector("#post-title");
+let postsData;
+
 function attachEvents() {
-    // TODO:
-}
+
+    const loadPostsBtn = document.querySelector("#btnLoadPosts");
+    loadPostsBtn.addEventListener("click", loadAllPosts);
+    const viewPostBtn = document.querySelector("#btnViewPost");
+    viewPostBtn.addEventListener("click", viewPostAndComments)
+};
+
+async function loadAllPosts() {
+    postsData = await ((await fetch(`${apiURL}posts`)).json())
+
+    Object.entries(postsData).map(([key, value]) => {
+        let option = document.createElement("option");
+        option.value = key;
+        option.textContent = postsData[key].title;
+        selectMenu.appendChild(option);
+    })
+};
+
+async function viewPostAndComments() {
+    clearOldData();
+
+    let currentPostID = selectMenu.value;
+
+    postTitleHeading.textContent = postsData[currentPostID].title;
+    postInfoOutput.textContent = postsData[currentPostID].body;
+
+    let allComments = await ((await fetch(`${apiURL}comments`)).json());
+
+    Object.values(allComments)
+        .filter(comment => comment.postId === currentPostID)
+        .map(element => {
+            let newListItem = document.createElement("li");
+            newListItem.id = element.id;
+            newListItem.textContent = element.text;
+
+            commentsList.appendChild(newListItem);
+        });
+};
+
+function clearOldData() {
+    postTitleHeading.textContent = "Post Details";
+    postInfoOutput.innerHTML = "";
+    commentsList.innerHTML = "";
+};
 
 attachEvents();
--- JS Front-End October 2023/JS HTTP and AJAX Exercise/06. Accordion/accordion.js
+++ JS Front-End October 2023/JS HTTP and AJAX Exercise/06. Accordion/accordion.js
@@ -1,3 +1,54 @@
-function solution() {
-    //TODO .....
+const apiURL = "http://localhost:3030/jsonstore/advanced/articles/";
+const sectionMain = document.querySelector("#main");
+
+async function solution() {
+    let data = await ((await fetch(`${apiURL}list`)).json());
+
+    for (let obj of data) {
+        let container = createDivElement("accordion");
+
+        let header = createDivElement("head");
+        let span = document.createElement("span");
+        span.textContent = obj.title;
+        let toggleBtn = document.createElement("button");
+        toggleBtn.classList.add("button");
+        toggleBtn.textContent = "More";
+        toggleBtn.id = obj._id;
+        toggleBtn.addEventListener("click", toggleExtraText);
+        header.appendChild(span);
+        header.appendChild(toggleBtn);
+
+        let extraInfo = createDivElement("extra");
+        let paragraph = document.createElement("p");
+
+        let extraText = await ((await fetch(`${apiURL}details/${obj._id}`)).json());
+        paragraph.textContent = extraText.content;
+
+        extraInfo.appendChild(paragraph);
+
+        container.appendChild(header);
+        container.appendChild(extraInfo);
+
+        sectionMain.appendChild(container);
+    };
 }
+
+function createDivElement(className) {
+    let div = document.createElement("div");
+    div.classList.add(className);
+
+    return div
+};
+
+function toggleExtraText(event) {
+    let currentBtn = event.target;
+    const isClosed = currentBtn.textContent === "More";
+
+    currentBtn.textContent = isClosed ? "Less" : "More";
+
+    let content = currentBtn.parentElement.parentElement.querySelector(".extra");
+    content.style.display = isClosed ? "block" : "none";
+
+};
+
+solution()
