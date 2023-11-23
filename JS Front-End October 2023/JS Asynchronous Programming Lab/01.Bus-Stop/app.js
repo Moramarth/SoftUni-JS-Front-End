5eadae3798b56888aff9b4fae345bc133a3a689d
George Lazarov <122516587+Moramarth@users.noreply.github.com>
2023-11-23 22:58:24 +0200

    JS Asynchronous Programming - Lab

--- JS Front-End October 2023/JS Asynchronous Programming Lab/01.Bus-Stop/app.js
+++ JS Front-End October 2023/JS Asynchronous Programming Lab/01.Bus-Stop/app.js
@@ -1,3 +1,20 @@
-function getInfo() {
-    // TODO:
+async function getInfo() {
+    const desiredID = document.querySelector("#stopId").value;
+    const resultTitle = document.querySelector("#stopName");
+    const resultList = document.querySelector("#buses");
+    resultList.innerHTML = "";
+
+    try {
+        response = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${desiredID}`);
+        parsedData = await response.json();
+
+        resultTitle.textContent = parsedData.name
+        Object.entries(parsedData.buses).forEach(([busId, time]) => {
+            let listItem = document.createElement("li")
+            listItem.textContent = `Bus ${busId} arrives in ${time} minutes`
+            resultList.appendChild(listItem)
+        })
+    } catch (_) {
+        resultTitle.textContent = "Error"
+    }
 }
